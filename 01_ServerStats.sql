e2b9baf8c80527333853db10bdf70e86d97fcab2
HEMANTH K ANDE <4361253+hemanthande@users.noreply.github.com>
2024-08-19 05:45:28 -0700

    Update 01_ServerStats.sql
    Updated  to add the Db file location --Commented

--- 01_ServerStats.sql
+++ 01_ServerStats.sql
@@ -8,9 +8,12 @@ GO
 
 SELECT DBNAME = DB_NAME(database_id)
 	,SizeMb = Cast(SUM(size * 8 / 1024.00) AS NUMERIC(32, 2))
-	,backupSQK = 'BACKUP DATABASE ' + DB_NAME(database_id) +' TO DISK = ''C:\SQLServer_Backups\'+DB_NAME(database_id) + '.bak''  WITH NOFORMAT,
-      NAME = ''' + @@SERVERNAME +' as of '+ CONVERT(NVARCHAR, GETDATE(), 101)+ '''; ' 
-FROM master.sys.master_files
-GROUP BY DB_NAME(database_id)
+	,backupSQK = 'BACKUP DATABASE ' + DB_NAME(database_id) +' TO DISK = ''C:\SQLServer_Backups\'+DB_NAME(database_id) + '.bak''  WITH NOFORMAT, NAME = ''' + @@SERVERNAME +' as of '+ CONVERT(NVARCHAR, GETDATE(), 101)+ '''; ' 
+	--,current_file_location = physical_name 
+FROM master.sys.master_files T
+WHERE database_id NOT IN (DB_ID('master'), DB_ID('model'), DB_ID('msdb'), DB_ID('tempdb'))
+GROUP BY DB_NAME(database_id)--, physical_name 
 --Order by Size desc, DBNAME
-ORDER BY SizeMb DESC
+--ORDER BY SizeMb DESC
+Order by DBNAME
+
